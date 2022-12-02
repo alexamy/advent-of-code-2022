ee2b41420c1d4320332c9a4b89275bce26844f83
Alexander Chaschin <ac@happynumbers.com>
2022-12-03 01:48:36 +0300

    add line groups

--- solutions/1-calorie-counting/index.test.ts
+++ solutions/1-calorie-counting/index.test.ts
@@ -1,5 +1,15 @@
 import { expect, it } from 'vitest';
+import { dedent } from 'ts-dedent';
+import { getLineGroups } from '.';
 
-it('falls', () => {
-  expect(true).toBe(false);
+it('groups numbers splitted by newline', () => {
+  const result = getLineGroups(dedent`
+    1
+
+    1
+  `)
+
+  expect(result).toEqual([['1'], ['1']]);
 });
+
+it.todo('sums consecutive numbers');
--- solutions/1-calorie-counting/index.ts
+++ solutions/1-calorie-counting/index.ts
@@ -1,1 +1,17 @@
-console.log('hey');
+export function getLineGroups(list: string): string[][] {
+  const groups: string[][] = [];
+
+  let idx = 0;
+  for(const line of list.split('\n')) {
+    if(line === '') {
+      idx += 1;
+      continue;
+    }
+
+    groups[idx]
+      ? groups[idx].push(line)
+      : (groups[idx] = [line])
+  };
+
+  return groups;
+}
