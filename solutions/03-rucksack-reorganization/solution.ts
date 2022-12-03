43d88a07eff7b59615e2e4dd528ead65c259cc1d
Alexander Chaschin <ac@happynumbers.com>
2022-12-03 13:00:42 +0300

    reorder methods

--- solutions/03-rucksack-reorganization/solution.ts
+++ solutions/03-rucksack-reorganization/solution.ts
@@ -5,35 +5,6 @@ import { sum } from 'radash';
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-export function sliceByHalf<T>(elements: T[]): T[][] {
-  assert(elements.length % 2 === 0, 'Row must have even count of elements.');
-
-  const half = elements.length / 2;
-  const first = elements.slice(0, half);
-  const second = elements.slice(half);
-
-  return [first, second];
-}
-
-export function findCommonElementInHalves(row: string): string {
-  const elements = row.split('');
-  const [first, second] = sliceByHalf(elements);
-
-  const [common] = first.filter(e => second.includes(e));
-  assert(common, `No common element found for ${row}.`);
-
-  return common;
-}
-
-export function getCharValue(char: string): number {
-  const isUppercase = char.toUpperCase() === char;
-  const value = alphabet.indexOf(char.toLowerCase())
-    + (isUppercase ? alphabet.length : 0)
-    + 1;
-
-  return value;
-}
-
 export function solve1(input: string): number {
   const lines = input.split('\n').filter(r => r);
   const result = pipe(
@@ -48,4 +19,33 @@ export function solve1(input: string): number {
 
 export function solve2(input: unknown): unknown {
   return undefined;
+}
+
+export function findCommonElementInHalves(row: string): string {
+  const elements = row.split('');
+  const [first, second] = sliceByHalf(elements);
+
+  const [common] = first.filter(e => second.includes(e));
+  assert(common, `No common element found for ${row}.`);
+
+  return common;
+}
+
+export function sliceByHalf<T>(elements: T[]): T[][] {
+  assert(elements.length % 2 === 0, 'Row must have even count of elements.');
+
+  const half = elements.length / 2;
+  const first = elements.slice(0, half);
+  const second = elements.slice(half);
+
+  return [first, second];
+}
+
+export function getCharValue(char: string): number {
+  const isUppercase = char.toUpperCase() === char;
+  const value = alphabet.indexOf(char.toLowerCase())
+    + (isUppercase ? alphabet.length : 0)
+    + 1;
+
+  return value;
 }
