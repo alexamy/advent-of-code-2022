ca43e2943d0bcc0a3e03df3b4fbc493d3c49ca2f
Alexander Chaschin <ac@happynumbers.com>
2022-12-05 14:23:57 +0300

    add transform input case

--- solutions/05-supply-stacks/solution.test.ts
+++ solutions/05-supply-stacks/solution.test.ts
@@ -1,6 +1,22 @@
 import { expect, it } from 'vitest';
 import dedent from 'ts-dedent';
-import { solve1, solve2 } from './solution';
+import { CargoData, solve1, solve2, transformInput } from './solution';
+
+it('transforms input', () => {
+  const result = solve1(dedent`
+    [D]
+    [N] [C]
+    1   2
+
+    move 1 from 1 to 2
+    move 2 from 2 to 1
+  `);
+
+  expect(transformInput(result)).toEqual<CargoData>({
+    crates: [['N', 'D'], ['C']],
+    designations: [{ count: 1, from: 1, to: 2 }, { count: 2, from: 2, to: 1 }],
+  });
+});
 
 it.skip('solves first part', () => {
   const result = solve1(dedent`
--- solutions/05-supply-stacks/solution.ts
+++ solutions/05-supply-stacks/solution.ts
@@ -1,3 +1,20 @@
+export type Stack = string[];
+
+export interface MoveDesignation {
+  count: number;
+  from: number;
+  to: number;
+}
+
+export interface CargoData {
+  crates: Stack[];
+  designations: MoveDesignation[];
+}
+
+export function transformInput(input: string): CargoData {
+
+}
+
 export function solve1(input: string): string {
   return '';
 }
