5b89fecb09de115dc9494affcdcdddd1f61eedaf
Alexander Chaschin <ac@happynumbers.com>
2022-12-10 18:05:27 +0300

    add more cases

--- solutions/06-tuning-trouble/solution.test.ts
+++ solutions/06-tuning-trouble/solution.test.ts
@@ -2,10 +2,14 @@ import { expect, it } from 'vitest';
 import dedent from 'ts-dedent';
 import { solve1, solve2 } from './solution';
 
-it.skip('solves first part', () => {
-  const result = solve1('mjqjpqmgbljsphdztnvjfqwrcgsmlb');
-
-  expect(result).toBe(7);
+it.skip.each<[string, number]>([
+  ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7],
+  ['bvwbjplbgvbhsrlpgdmjqwftvncz', 5],
+  ['nppdvjthqldpwncqszvftbrmjlhg', 6],
+  ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10],
+  ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11],
+])('solves first part', ([message, result]) => {
+  expect(solve1(message)).toBe(result);
 });
 
 it.skip('solves second part', () => {
