b75941a8ccdd74d4d3ec01b243a37117cce185cd
Alexander Chaschin <ac@happynumbers.com>
2022-12-11 00:05:33 +0300

    add first solution

--- solutions/06-tuning-trouble/solution.res
+++ solutions/06-tuning-trouble/solution.res
@@ -1,4 +1,15 @@
 @genType
 let solve1 = (message: string): int => {
-  0
+  let indexes = Belt.Array.range(0, Js.String.length(message))
+  let target = Belt.Array.getBy(indexes, (idx) => {
+    let fours = Js.String.slice(~from=idx, ~to_=idx+4, message)
+    let chars = Js.String.split("", fours)
+    let uniqueChars = Belt.Set.String.fromArray(chars)
+    Belt.Set.String.size(uniqueChars) == 4
+  })
+
+  switch target {
+  | None => raise(Not_found)
+  | Some(index) => index + 4
+  }
 }
--- solutions/06-tuning-trouble/solution.test.ts
+++ solutions/06-tuning-trouble/solution.test.ts
@@ -1,13 +1,13 @@
 import { expect, it } from 'vitest';
 import { solve1 } from './solution.gen';
 
-it.skip.each<[string, number]>([
+it.each<[string, number]>([
   ['mjqjpqmgbljsphdztnvjfqwrcgsmlb', 7],
   ['bvwbjplbgvbhsrlpgdmjqwftvncz', 5],
   ['nppdvjthqldpwncqszvftbrmjlhg', 6],
   ['nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10],
   ['zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11],
-])('solves first part', ([message, result]) => {
+])('solves first part', (message, result) => {
   expect(solve1(message)).toBe(result);
 });
 
