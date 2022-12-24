24bf09e0cf77f87fe43a99bb1a0385d4535f0a44
Alexander Chaschin <ac@happynumbers.com>
2022-12-24 13:59:24 +0300

    add rescript template

---
+++ solutions/template-rescript/SolutionXX.gen.ts
@@ -0,0 +1,11 @@
+/* TypeScript file generated from SolutionXX.res by genType. */
+/* eslint-disable import/first */
+
+
+// @ts-ignore: Implicit any on import
+import * as SolutionXXBS__Es6Import from './SolutionXX.bs';
+const SolutionXXBS: any = SolutionXXBS__Es6Import;
+
+export const solve1: <T1>(_input:string) => (null | undefined | T1) = SolutionXXBS.solve1;
+
+export const solve2: <T1>(_input:string) => (null | undefined | T1) = SolutionXXBS.solve2;
