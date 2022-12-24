0b12fce6d15ec36ced479eead6ef401c336dc6aa
Alexander Chaschin <ac@happynumbers.com>
2022-12-24 13:45:14 +0300

    add solution 1

--- solutions/answers/answer.test.ts
+++ solutions/answers/answer.test.ts
@@ -1,8 +1,21 @@
 import { test, expect } from 'vitest';
 import { readFile } from 'fs-extra';
+import * as S1 from '../01-calorie-counting/solution';
 import * as S5 from '../05-supply-stacks/Solution05.gen';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { encrypt, decrypt } from './cipher';
+
+test('Solution 01', async () => {
+  const file = await readFile('solutions/01-calorie-counting/input.txt');
+  const input = file.toString();
+
+  const result1 = 'U2FsdGVkX19GwneJUSfz4kzJaumaIvYhc7TuyIeMBXg=';
+  expect(S1.solve1(input).toString()).toBe(decrypt(result1));
+
+  const result2 = 'U2FsdGVkX1/PI85EaUG/TsYgvNA0w2xNjZpoDiIH4jQ=';
+  expect(S1.solve2(input).toString()).toBe(decrypt(result2));
+});
+
 
 test('Solution 05', async () => {
   const file = await readFile('solutions/05-supply-stacks/input.txt');
