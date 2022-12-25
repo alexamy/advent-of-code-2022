c085712b71a5be57150f32199a3ed9cde677ef5b
Alexander Chaschin <ac@happynumbers.com>
2022-12-25 16:23:39 +0300

    add second part

--- solutions/08-treetop-tree-house/solution.test.ts
+++ solutions/08-treetop-tree-house/solution.test.ts
@@ -1,5 +1,5 @@
 import { expect, it } from 'vitest';
-import { solve1 } from './Solution08.gen';
+import { solve1, solve2 } from './Solution08.gen';
 import dedent from 'ts-dedent';
 
 it('solves first part', () => {
@@ -12,4 +12,16 @@ it('solves first part', () => {
   `);
 
   expect(result).toBe(21);
+});
+
+it('solves second part', () => {
+  const result = solve2(dedent`
+    30373
+    25512
+    65332
+    33549
+    35390
+  `);
+
+  expect(result).toBe(8);
 });
